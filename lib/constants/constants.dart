626e42b9df42f766dc3fa4a2729cc6bfe6e8c51a
imanshbb <www.seyyediman1234@gmail.com>
2023-08-11 12:02:22 +0330

    create constans

---
+++ lib/constants/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Constant {
+  static Color primeryColor = Colors.green;
+  static Color blackColor = Colors.black;
+
+  static String titleOne = 'گیاهان را بهتر از قبل درک کنید';
+  static String descriptionOne =
+      'در مورد نگه داری گل و گیاهان می توانی اطلاعات کسب کنی';
+  static String titleTow = 'با گیاهان جدید آشنا شو';
+  static String descriptionTow =
+      'رز مشکی یا گل رز دوست داری؟ اینجا می تونی پیداش کنی';
+  static String titleTree = 'با یک گل بهار نمی شود، گل بکار';
+  static String descriptionTree =
+      'هر گلی نیاز داشته باشید در این اپلیکشن پیدا میکنید';
+}
