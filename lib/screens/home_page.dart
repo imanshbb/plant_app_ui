3fc74f5bdfdb8c80b85b7aff5b1d92b59eb4255a
imanshbb <www.seyyediman1234@gmail.com>
2023-08-15 18:54:03 +0330

    create product 1

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_6/constants/constants.dart';
+import 'package:flutter_application_6/models/plant.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int selectindex = 0;
+
   List<String> plantType = [
     '| پیشنهادی |',
     '| آپارتمانی |',
@@ -17,6 +19,11 @@ class _HomePageState extends State<HomePage> {
     '| گل باغچه ای |',
     '| گل سمی |',
   ];
+  List<Plant> plant = Plant.plantList;
+  bool isFavorit1(favorit) {
+    return !favorit;
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -90,6 +97,55 @@ class _HomePageState extends State<HomePage> {
                               : Constant.blackColor,
                         ),
                       ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            //product One
+
+            SizedBox(
+              height: size.height * 0.3,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                reverse: true,
+                itemCount: plant.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 200.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Constant.primeryColor.withOpacity(0.8),
+                    ),
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          top: 10.0,
+                          right: 20.0,
+                          child: Container(
+                            width: 40.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50.0),
+                              color: Colors.white,
+                            ),
+                            child: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isFavorit1(plant[index].isFavorated);
+                                });
+                              },
+                              icon: Icon(
+                                plant[index].isFavorated == true
+                                    ? Icons.favorite
+                                    : Icons.favorite_border_outlined,
+                                size: 20.0,
+                              ),
+                              color: Constant.primeryColor,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   );
                 },
