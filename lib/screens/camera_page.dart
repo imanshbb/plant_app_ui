c5a7182d7bd79cfa92940bee946018c517531718
imanshbb <www.seyyediman1234@gmail.com>
2023-08-18 13:19:01 +0330

    finish camera page

--- lib/screens/camera_page.dart
+++ lib/screens/camera_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_6/constants/constants.dart';
 import 'package:flutter_application_6/screens/qr_overlay.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -10,24 +12,85 @@ class Camera extends StatefulWidget {
 }
 
 class _CameraState extends State<Camera> {
+  MobileScannerController cameraController = MobileScannerController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          MobileScanner(
-            onDetect: (capture) {
-              final List<Barcode> barcodes = capture.barcodes;
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            MobileScanner(
+              onDetect: (capture) {
+                final List<Barcode> barcodes = capture.barcodes;
 
-              for (final barcode in barcodes) {
-                debugPrint('Barcode found! ${barcode.rawValue}');
-              }
-            },
-          ),
-          QRScannerOverlay(
-            overlayColour: Colors.black.withOpacity(0.4),
-          ),
-        ],
+                for (final barcode in barcodes) {
+                  debugPrint('Barcode found! ${barcode.rawValue}');
+                }
+              },
+            ),
+
+            QRScannerOverlay(
+              overlayColour: Colors.black.withOpacity(0.4),
+            ),
+            //appbar
+            Positioned(
+              right: 20.0,
+              left: 20.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  // X icon
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      width: 40.0,
+                      height: 40.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Constant.primeryColor.withOpacity(0.3),
+                      ),
+                      child: const Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  //torch icon
+                  Container(
+                    width: 40.0,
+                    height: 40.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Constant.primeryColor.withOpacity(0.3),
+                    ),
+                    child: IconButton(
+                      onPressed: () => cameraController.toggleTorch(),
+                      icon: ValueListenableBuilder(
+                        valueListenable: cameraController.torchState,
+                        builder: (context, state, child) {
+                          switch (state) {
+                            case TorchState.off:
+                              return const Icon(
+                                Icons.flash_off,
+                                color: Colors.white,
+                              );
+                            case TorchState.on:
+                              return const Icon(
+                                Icons.flash_on,
+                                color: Colors.yellow,
+                              );
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
